f19e7312f13872766f240c5e231f1cf201998204
Artemiy Ivanov <solvextiger@gmail.com>
2022-02-13 01:00:36 +0300

    added a test for pokemon method and added schemes for scripts

--- exercise-2/cs_spec.rb
+++ exercise-2/cs_spec.rb
@@ -1,14 +1,14 @@
 require 'rspec'
 require './cs.rb'
 
-RSpec.describe "last_2_char method" do
+RSpec.describe "last_2_chars method" do
     it "#return reverse word" do
-        expect(last_2_char("hello")).to eq("olleh")
+        expect(last_2_chars("hello")).to eq("olleh")
     end
     it "#return 2 to the power of the word length" do 
-        expect(last_2_char("GOCS")).to eq(16)
+        expect(last_2_chars("GOCS")).to eq(16)
     end
     it "#return 2 to the power of the word length" do 
-        expect(last_2_char("GOCS")).to eq(16)
+        expect(last_2_chars("GOCS")).to eq(16)
     end
 end
--- exercise-2/pokemons_spec.rb
+++ exercise-2/pokemons_spec.rb
@@ -2,5 +2,16 @@ require 'rspec'
 require './pokemons.rb'
 
 RSpec.describe "ask_about_pokemons" do
+    it "#return a pokemon list" do
+        allow_any_instance_of(Kernel).to receive(:gets).and_return(2,'Pikachu','Yellow', 'Charizard','Orange')
+
+        expect(ask_about_pokemons).to eq([{name: 'Pikachu', color: 'Yellow'}, {name: 'Charizard', color: 'Orange'}])
+    end
+    it "#return an empty pokemon list" do
+        allow_any_instance_of(Kernel).to receive(:gets).and_return(0)
+
+        expect(ask_about_pokemons).to eq([])
+    end
+    
    
 end
