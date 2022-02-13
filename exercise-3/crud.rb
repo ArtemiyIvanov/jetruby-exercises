8b4ec4c1349f91dc12223b44efcc0e3639f371cd
Artemiy Ivanov <solvextiger@gmail.com>
2022-02-13 22:23:23 +0300

    added crud methods

---
+++ exercise-3/crud.rb
@@ -0,0 +1,50 @@
+FILE_PATH = 'main.txt'
+BUFFER = 'buffer.txt'
+
+def index
+    File.foreach(FILE_PATH) {|line| puts line}
+end
+
+def where(word)
+    File.foreach(FILE_PATH).with_index do |line, index|
+        if line.include?(word)
+            @line_id = index
+        end
+    end
+    @line_id
+end
+
+def update(id, word)
+    file = File.open(BUFFER, 'w')
+    File.foreach(FILE_PATH).with_index do |line, index|
+        file.puts(index == id ? word : line)
+    end
+    file.close
+
+    File.write(FILE_PATH, File.read(BUFFER))
+
+    File.delete(BUFFER) if (File.exist?(BUFFER))
+end
+
+def delete(id)
+    file = File.open(BUFFER, 'w')
+    File.foreach(FILE_PATH).with_index do |line, index|
+        index == id ? next : file.puts(line)
+    end
+    file.close
+
+    File.write(FILE_PATH, File.read(BUFFER))
+
+    File.delete(BUFFER) if (File.exist?(BUFFER))
+end
+
+def find(id)
+    File.foreach(FILE_PATH).with_index do |line, index|
+        if index == id
+            @line = line
+        end
+    end
+    @line
+end
+
+puts find(2)
