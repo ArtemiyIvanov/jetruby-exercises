2bcd3a01da33a0e55db7f2876fadd62a36a05e61
Artemiy Ivanov <solvextiger@gmail.com>
2022-02-20 17:36:24 +0300

    added test for task about students

--- exercise-3/students.rb
+++ exercise-3/students.rb
@@ -7,8 +7,9 @@ def where(age, students)
     end
     return id
 end
-
 STUDENTS_FILE_PATH = 'students.txt'
+RESULT_FILE_PATH = 'result.txt'
+def main
 string = 
 <<-STUDENTS
 Tom Phelps 22
@@ -22,37 +23,36 @@ Bruce Wayne 25
 Natasha Romanoff 21
 Eddie Brock 20
 STUDENTS
-RESULT_FILE_PATH = 'result.txt'
-
-File.write(STUDENTS_FILE_PATH, string)
-students = []
-File.foreach(STUDENTS_FILE_PATH) do |line|
-    student = {}
-    split_line = line.split
-    student[:first_name] = split_line[0]
-    student[:second_name] = split_line[1]
-    student[:age] = split_line[2]
-    students.push(student)
-end
-
-result = []
-begin
-
-    puts "Enter an age of student"
-    age = gets.to_i
-    break if age == -1
-    id = where(age.to_s, students)
-    for i in id
-        result.push(students[i])
+    File.write(STUDENTS_FILE_PATH, string)
+    students = []
+    File.foreach(STUDENTS_FILE_PATH) do |line|
+        student = {}
+        split_line = line.split
+        student[:first_name] = split_line[0]
+        student[:second_name] = split_line[1]
+        student[:age] = split_line[2]
+        students.push(student)
     end
-end while students.length != result.length
 
-result_file = File.open(RESULT_FILE_PATH, 'w')
+    result = []
+    begin
 
-result.each do |student|
-    result_file.puts(student[:first_name] + ' ' + student[:second_name] + ' ' + student[:age])
+        puts "Enter an age of student"
+        age = gets
+        age = age.to_i
+        break if age == -1
+        id = where(age.to_s, students)
+        for i in id
+            result.include?(students[i]) ? next : result.push(students[i])
+        end
+    end while students.length != result.length
+
+    result_file = File.open(RESULT_FILE_PATH, 'w')
+
+    result.each do |student|
+        result_file.puts(student[:first_name] + ' ' + student[:second_name] + ' ' + student[:age])
+    end
+    result_file.close
+
+    return File.read(RESULT_FILE_PATH)
 end
-result_file.close
-
-puts File.read(RESULT_FILE_PATH)
-
